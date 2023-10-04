d7088ed6164141ac4e063596be90c8c5c124b410
TatianaHoza <tatiana-15@tut.by>
2023-10-05 00:01:22 +0300

    add task 14-15

---
+++ Add/Validator.java
@@ -0,0 +1,20 @@
+﻿package Add;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Arrays;
+import Exceptions.*;
+
+public class Validator {
+
+    public void validate (String [] data){
+
+        StringBuilder sb = new StringBuilder();
+        boolean flag = true;
+
+        for (int i=0; i < data.length; i++){
+            try{
+                if (i==0)
+                    isValidName(data[i]);
+                if (i==1)
